818289006398a87784ffcfe7b1fa32e8985e50ec
Alexei Torchinsky <alextor1005@gmail.com>
2023-04-29 00:53:39 +0300

    feat: add text area and keybord rendering

--- script.js
+++ script.js
@@ -1,24 +1,43 @@
 // window.onload = function() {
 //   // code to generate virtual keyboard goes here
 // };
+const textArea = document.createElement('textarea');
+textArea.className = 'textarea';
+document.body.append(textArea);
 
 const keys = [
-  ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', "'\\", 'Backspace']
-  ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p'],
-  ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l'],
-  ['z', 'x', 'c', 'v', 'b', 'n', 'm']
+  ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', '\\', '←'],
+  ['Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', 'Del'],
+  ['Caps Lock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', "'", 'Enter'],
+  ['Shift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', 'Shift'],
+  ['Ctrl', 'Alt', ' ', 'Alt', 'Ctrl'],
 ];
-var keyboard = document.createElement('div');
+const keyboard = document.createElement('div');
 keyboard.classList.add('keyboard');
 
-for (var i = 0; i < keys.length; i++) {
-  var row = document.createElement('div');
+for (let i = 0; i < keys.length; i += 1) {
+  const row = document.createElement('div');
   row.classList.add('row');
 
-  for (var j = 0; j < keys[i].length; j++) {
-    var key = document.createElement('button');
+  for (let j = 0; j < keys[i].length; j += 1) {
+    const key = document.createElement('button');
     key.classList.add('key');
     key.textContent = keys[i][j];
+    if (key.textContent === ' ') {
+      key.classList.add('key-space');
+    }
+    if (key.textContent === 'Tab' || key.textContent === 'Del') {
+      key.classList.add('key-tab');
+    }
+    if (key.textContent === 'Caps Lock' || key.textContent === 'Enter') {
+      key.classList.add('key-enter');
+    }
+    if (key.textContent === 'Shift') {
+      key.classList.add('key-shift');
+    }
+    if (key.textContent === 'Ctrl' || key.textContent === 'Alt') {
+      key.classList.add('key-ctrl');
+    }
     row.appendChild(key);
   }
 
